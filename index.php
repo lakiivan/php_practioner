043d8ab86c1bfdabe2eac2ce96035ffb7ae3ea1a
lakiivan <ivanzlazic@gmail.com>
2022-08-28 13:20:01 +0200

    data sent to git

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $task = [
+        'title' => 'install ios software',
+        'due' => 'tomrrow',
+        'assigned_to' => 'Ivan',
+        'isCompleted' => False
+
+    ];
+
+    require 'index.view.php';
